7236ac509f9e71997f4ea2f1888a26b20604fe50
Arsenii <magistar035@gmail.com>
2024-12-25 08:57:23 +0300

    ысфду

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+
 const questions = [
     {
         question: "В каком университете ты учишься?",
